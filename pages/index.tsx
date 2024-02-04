3031a5a962a66fb93b2aa1129842130c6143cf0f
lyc8503 <me@lyc8503.site>
2024-02-04 11:46:09 +0800

    fix #6: style change

--- pages/index.tsx
+++ pages/index.tsx
@@ -49,23 +49,23 @@ export default function Home({
         )}
 
         <Divider mt="lg" />
-        <Center>
-          <Text size="xs" mt="xs" mb="xs">
-            Open-source monitoring and status page powered by{' '}
-            <a href="https://github.com/lyc8503/UptimeFlare" target="_blank">
-              Uptimeflare
-            </a>{' '}
-            and{' '}
-            <a href="https://www.cloudflare.com/" target="_blank">
-              Cloudflare
-            </a>
-            , made with ❤ by{' '}
-            <a href="https://github.com/lyc8503" target="_blank">
-              lyc8503
-            </a>
-            .
-          </Text>
-        </Center>
+        <Text size="xs" mt="xs" mb="xs" style={{
+          textAlign: 'center'
+        }}>
+          Open-source monitoring and status page powered by{' '}
+          <a href="https://github.com/lyc8503/UptimeFlare" target="_blank">
+            Uptimeflare
+          </a>{' '}
+          and{' '}
+          <a href="https://www.cloudflare.com/" target="_blank">
+            Cloudflare
+          </a>
+          , made with ❤ by{' '}
+          <a href="https://github.com/lyc8503" target="_blank">
+            lyc8503
+          </a>
+          .
+        </Text>
       </main>
     </>
   )
