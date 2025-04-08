f974ad44284cffbcfa1ea715b2027e234db2f82c
lyc8503 <me@lyc8503.net>
2025-04-08 10:45:38 +0800

    WIP: check proxy

---
+++ proxy/api/index.js
@@ -0,0 +1,15 @@
+// This is a Node.js implementation of status monitoring
+
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`App listening on port ${port}`)
+})
+
+module.exports = app;
