43890eeba68573489bd644efb77dc96592793cda
lyc8503 <me@lyc8503.net>
2025-04-11 23:30:34 +0800

    fix: pages colo

--- pages/api/check.ts
+++ pages/api/check.ts
@@ -1,5 +1,6 @@
 import { NextRequest } from "next/server"
 import { getStatus } from "@/worker/src/monitor"
+import { getWorkerLocation } from "@/worker/src/util"
 import { MonitorTarget } from "@/uptime.types"
 
 export const runtime = 'edge'
@@ -7,7 +8,7 @@ export const runtime = 'edge'
 // Check proxy if moved from Workers to Pages, https://github.com/lyc8503/UptimeFlare/issues/86#issuecomment-2655187257
 export default async function handler(req: NextRequest): Promise<Response> {
   const target = await req.json<MonitorTarget>()
-  const colo = req.headers.get("cf-ray")?.split("-")[1]
+  const colo = await getWorkerLocation()
   console.log("Check proxy running at " + colo + ", Target: " + target)
   return new Response(
     JSON.stringify({
